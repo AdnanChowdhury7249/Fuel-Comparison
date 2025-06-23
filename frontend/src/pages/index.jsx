7d78467cdd46ce3b0f378cf40a868da297c5adb8
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-06-23 16:32:37 +0100

    updated frontend allow user to sort by cheapest and nearest

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -6,6 +6,8 @@ import FuelCard from "../components/FuelCard";
 function Compare() {
   const [postcode, setPostcode] = useState("")
   const [result, setResult] = useState(null)
+  const [sortBy, setSortBy] = useState("Nearest")
+  const [fuelType, setFuelType] = useState("E10");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,6 +21,19 @@ function Compare() {
 
     }
   }
+  let sortedStations = [...(result?.stations || [])]
+
+  if (sortBy === "Nearest") {
+    sortedStations.sort((a, b) => a.Distance_km - b.Distance_km)
+
+  } else if (sortBy === "Cheapest") {
+    sortedStations.sort((a, b) => {
+      const aPrice = a.prices[fuelType] ?? Infinity;
+      const bPrice = b.prices[fuelType] ?? Infinity;
+      return aPrice - bPrice;
+    })
+  }
+
   return (
     <div className="">
       <form onSubmit={handleSubmit}>
@@ -27,11 +42,29 @@ function Compare() {
           onChange={(e) => setPostcode(e.target.value)}
           placeholder="Enter postcode"
         />
-        <button type="submit">Compare</button>
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+          className="border p-2 rounded mb-4"
+        >
+          <option value="Nearest">Sort by Nearest</option>
+          <option value="Cheapest">Sort by Cheapest (E10)</option>
+        </select>
+        <select
+          value={fuelType}
+          onChange={(e) => setFuelType(e.target.value)}
+          className="border p-2 rounded mb-4 ml-2"
+        >
+          <option value="E10">Petrol</option>
+          <option value="E5">Premium Petrol</option>
+          <option value="B7">Diesel</option>
+          <option value="SDV">Super Diesel</option>
+        </select>
+        <button type="submit" className="hover:bg-gray-200 rounded p-1 cursor-pointer">Compare</button>
       </form>
       <div className="w-full px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10 w-full max-w-5xl mx-auto ">
-          {result && result.stations.map((station) => (
+          {sortedStations.map((station) => (
             <FuelCard key={station.site_id} station={station} />
           ))}
         </div>
