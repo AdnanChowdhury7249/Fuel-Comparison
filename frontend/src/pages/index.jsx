b0bb7f823c8864657a3935fd8cae1c895edbc973
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-06-23 12:50:13 +0100

    removed unused files

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -30,7 +30,7 @@ function Compare() {
         <button type="submit">Compare</button>
       </form>
       <div className="w-full px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10 w-full max-w-5xl mx-auto pl-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10 w-full max-w-5xl mx-auto ">
           {result && result.stations.map((station) => (
             <FuelCard key={station.site_id} station={station} />
           ))}
